a839a50f05c2eee02a033f9f798395bb2cf226fc
zhangziyu <zhang.ziyu@zhaopin.com.cn>
2018-08-28 13:40:12 +0800

    FutureTaskTest

---
+++ src/test/java/thread/FutureTaskTest.java
@@ -0,0 +1,55 @@
+package thread;
+
+import java.util.concurrent.*;
+
+public class FutureTaskTest {
+    public static void main(String[] args) {
+        //ExecutorService 方式：
+        //ExecutorService executor = Executors.newCachedThreadPool();
+        ExecutorService executor = Executors.newFixedThreadPool(20);
+        for (int i=0;i<10;i++) {
+            String tasknum = "task" + i;
+            Task task = new Task(tasknum);
+            FutureTask<String> futureTask = new FutureTask<String>(task);
+            System.out.println(tasknum + "子线程运行完毕");
+            executor.submit(futureTask);
+            executor.shutdown();
+
+            //Thread 方式：
+            /*Task task = new Task();
+            FutureTask<Integer> futureTask = new FutureTask<Integer>(task);
+            Thread thread = new Thread(futureTask);
+            thread.start();*/
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            try {
+                System.out.println(tasknum+"运行结果" + futureTask.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(tasknum+"任务执行完毕");
+        }
+    }
+}
+class Task implements Callable<String> {
+
+    private String parameter;
+
+    public Task(String parameter) {
+        this.parameter = parameter;
+    }
+
+    @Override
+    public String call() throws Exception {
+        System.out.println(parameter+"线程在运行");
+        Thread.sleep(3000);
+        return parameter;
+    }
+}
